f9b21654876df7b9f07c6aaa244b07babfa64674
75047540 <75047540@continental.edu.pe>
2024-09-02 16:53:30 -0500

    Implementacion del MINIMO COMUN MILTIPLO(mcm)

--- archivo file.py
+++ archivo file.py
@@ -7,11 +7,16 @@ class numeroEntero:
             remainder = x % y
             x = y
             y = remainder
-
         return x
+
+    def mcm(self, a, b):
+        # mcd*mcm = a * b
+        return a*b/self.MDC(a,b)
+
 if __name__ == '__main__':
     numero1=int(input("primer numero: "))
     numero2=int(input("segundo numero: "))
 
     operacion = numeroEntero()
     print(f"MCD de {numero1} {numero2} es {operacion.MDC(numero1,numero2)}" )
+    print(f"MCM de {numero1} {numero2} es {operacion.mcm(numero1, numero2)}")
